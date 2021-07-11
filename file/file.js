08fbe372a766bd54b865672ec69bb65812918083
David Groomes <dwgroomes@gmail.com>
2021-07-11 17:14:35 -0500

    Copy a file using read/write streams

--- file/file.js
+++ file/file.js
@@ -1,11 +1,32 @@
 #!/usr/bin/env node
 
-const {readFileSync} = require('fs')
-
+// Read a file
 {
+    const {readFileSync} = require('fs')
     let file = 'README.md';
     console.log(`Reading file synchronously: ${file}...`)
     let buffer = readFileSync(file)
     let fileContent = new TextDecoder().decode(buffer)
     console.log(`File content:\n${fileContent}`)
+}
+
+// Copy a file using read and write streams.
+{
+    const {createReadStream, createWriteStream} = require('fs')
+    let sourceFile = 'README.md';
+    let targetFile = 'copy-of-README.md';
+    console.log(`Copying the file '${sourceFile}' asynchronously to '${targetFile}' using read and write streams...`)
+    let readStream = createReadStream(sourceFile);
+    let writeStream = createWriteStream(targetFile);
+    let decoder = new TextDecoder()
+    readStream.on('data', chunk => {
+        let decoded = decoder.decode(chunk);
+        console.log(`Read a chunk: ${decoded}`)
+        writeStream.write(chunk)
+    })
+
+    readStream.on('close', () => {
+        writeStream.close()
+        console.log("Successfully copied the file.")
+    })
 }
